761d8bb25d10f945394d5aef16c2b9074dd1c565
Matteo <ferrara.matteo@yahoo.it>
2015-12-04 12:47:14 +0000

    step due

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,21 @@
+# Given(/^the following movies exist:$/) do |table|
+#     table.hashes.each do |movie|
+#         Movie.create({:title => movie[:title], rating => movie[:rating], :release_date => movie[:release_date], :director => movie[:director]})
+#     end
+# end
+
+Given(/^the following movies exist:$/) do |table|
+    table.hashes.each do |movie|
+        Movie.create!(movie)
+    end
+end
+
+When(/^I go to the edit page for "(.*?)"$/) do |name|
+    @movie = Movie.find_by(title: name)
+    visit edit_movie_path(@movie.it)
+end
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |name, director|
+    expect(Movie.find_by(title: name).to eq(director))
+end
+
